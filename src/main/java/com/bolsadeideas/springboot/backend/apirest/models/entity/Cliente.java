e7553795a5a47b2a925848e9c28daf4ebb22381e
leanysp <leanysp@gmail.com>
2020-07-16 12:36:41 -0500

    first commit

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,108 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	private String apellido;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+	
+	@PrePersist
+	public void PrePresist() {
+		createAt = new Date();
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the apellido
+	 */
+	public String getApellido() {
+		return apellido;
+	}
+	/**
+	 * @param apellido the apellido to set
+	 */
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the createAt
+	 */
+	public Date getCreateAt() {
+		return createAt;
+	}
+	/**
+	 * @param createAt the createAt to set
+	 */
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,21 @@
+/* Populate tables */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Leanys','Pineda','leanysp@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ho','Tolosa','htolosam@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Aria','Tolosa','leanysp@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Sebastian','Pineda','sebastian@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Oscar','Pineda','oscar@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jader','Tolosa','jader@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dariel','Pineda','dariel@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jaime','Pineda','jaime@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ana','Ortega','ana@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Victor','Pineda','victor@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Norma','Pineda','norma@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Yuranis','Ortega','yuranis@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Sofia','Correa','sofia@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Daniel','Medina','daniel@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Miguel','Medina','miguel@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Samuel','Medina','samuel@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Obeimar','Ortega','obeimar@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Cristian','Ortega','cristian@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Wendy','Ortega','wendy@gmail.com','2020-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('David','Ortega','david@gmail.com','2020-07-07');
